a708095d6cddcd08822fe9b7812aa354758ff5ab
Eric Van Wyk <evw@cs.umn.edu>
2016-10-29 06:46:08 -0500

    initial regex, from old copy

---
+++ mda_test/MDA.sv
@@ -0,0 +1,16 @@
+grammar edu:umn:cs:melt:exts:ableC:regex:mda_test;
+
+import edu:umn:cs:melt:ableC:host;
+
+copper_mda testMatching(ablecParser) {
+  edu:umn:cs:melt:exts:ableC:regex:regexMatching;
+}
+
+--copper_mda testMatchingVerbose(ablecParser) {
+--  edu:umn:cs:melt:exts:ableC:regex:regexMatchingVerbose;
+--}
+
+copper_mda testLiterals(ablecParser) {
+  edu:umn:cs:melt:exts:ableC:regex:regexLiterals;
+}
+
---
+++ regexMatchingVerbose/Regex.sv
@@ -0,0 +1,25 @@
+grammar edu:umn:cs:melt:exts:ableC:regex:regexMatchingVerbose;
+
+imports edu:umn:cs:melt:ableC:concretesyntax as cnc;
+imports edu:umn:cs:melt:ableC:abstractsyntax as abs;
+imports silver:langutil only ast;
+
+import edu:umn:cs:melt:exts:ableC:regex;
+
+-- Spurious import, to trigger the tests on build.
+import edu:umn:cs:melt:exts:ableC:regex:mda_test;
+
+marking terminal RegexMatch_t 'match' lexer classes {Ckeyword} ;
+terminal Against_t 'against' ;
+
+concrete productions top::cnc:PrimaryExpr_c
+| 'match' e::cnc:Expr_c 'against' r::RX_c
+  { top.ast = regexMatch(e.ast, r.ast, location=top.location); }
+
+nonterminal RX_c with ast<abs:Expr>, location;
+
+concrete production rx_c
+rx::RX_c ::= d1::RegexBegin_t  r::Regex_R  d2::RegexEnd_t
+layout {}
+{ rx.ast = regexLiteralExpr("\"" ++ r.regString ++ "\"", location=d1.location);
+}
