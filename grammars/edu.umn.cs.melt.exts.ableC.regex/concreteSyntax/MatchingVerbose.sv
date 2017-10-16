64fceb671dffe2db738376f70512f87f688e7950
StephenPatrick <sythe2o0@gmail.com>
2017-10-16 14:09:34 -0500

    Verbose matching works but fails MDA

--- grammars/edu.umn.cs.melt.exts.ableC.regex/concreteSyntax/MatchingVerbose.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.regex/concreteSyntax/MatchingVerbose.sv
@@ -1,18 +1,20 @@
 grammar edu:umn:cs:melt:exts:ableC:regex:concretesyntax;
 
--- This fails to compile-- RegexBegin is in a non bridge production position.
+marking terminal RegexMatchVerbose_t 'match' lexer classes {Ckeyword} ;
+terminal Against_t 'against' ;
 
--- marking terminal RegexMatchVerbose_t 'match' lexer classes {Ckeyword} ;
--- terminal Against_t 'against' ;
+terminal VerboseStart_t '/';
+terminal VerboseEnd_t '/';
 
--- concrete productions top::cnc:PrimaryExpr_c
--- | 'match' e::cnc:Expr_c 'against' r::RX_c
---   { top.ast = regexMatch(e.ast, r.ast, location=top.location); }
+-- This usage of against fails MDA
+concrete productions top::cnc:PrimaryExpr_c
+| 'match' e::cnc:Expr_c 'against' r::RX_c
+  { top.ast = regexMatch(e.ast, r.ast, location=top.location); }
 
--- nonterminal RX_c with ast<abs:Expr>, location;
+nonterminal RX_c with ast<abs:Expr>, location;
 
--- concrete production rx_c
--- rx::RX_c ::= d1::RegexBegin_t  r::Regex_R  d2::RegexEnd_t
--- layout {}
--- { rx.ast = regexLiteralExpr("\"" ++ r.regString ++ "\"", location=d1.location);
--- }
+concrete production rx_c
+rx::RX_c ::= d1::VerboseStart_t  r::Regex_R  d2::VerboseEnd_t
+layout {}
+{ rx.ast = regexLiteralExpr("\"" ++ r.regString ++ "\"", location=d1.location);
+}
