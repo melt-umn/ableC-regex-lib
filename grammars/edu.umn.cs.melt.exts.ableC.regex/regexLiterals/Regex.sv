aacf9adc743894320b28bd991e8467c616dd8a1d
Lucas Kramer <krame505@umn.edu>
2020-02-11 15:08:03 -0600

    Fix errors in last commit

--- grammars/edu.umn.cs.melt.exts.ableC.regex/regexLiterals/Regex.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.regex/regexLiterals/Regex.sv
@@ -9,8 +9,8 @@ import edu:umn:cs:melt:exts:ableC:regex;
 import edu:umn:cs:melt:exts:ableC:regex:mda_test;
 
 -- 'Operator' is maybe the closest designation for these? IDK.
-marking terminal RegexBegin_t '/' lexer classes Operator;
-terminal RegexEnd_t '/' lexer classes Operator;
+marking terminal RegexBegin_t '/' lexer classes cnc:Operator;
+terminal RegexEnd_t '/' lexer classes cnc:Operator;
 
 concrete production regex_c
 e::cnc:PrimaryExpr_c ::= d1::RegexBegin_t  r::Regex_R  d2::RegexEnd_t
